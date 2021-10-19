eca22e719a217d32687e805d9e40b482f9f4e051
A. Korobetskaya <kornast@yandex.ru>
2021-10-19 21:20:33 +0400

    Add project files.

---
+++ Enumerators/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Enumerators
+{
+    class Week : IEnumerable
+    {
+        readonly string[] days = { "пн", "вт", "ср", "чт", "пт", "сб", "вс" };
+
+        public IEnumerator GetEnumerator()
+        {
+            return days.GetEnumerator();
+        }
+    }
+
+    class RandomSequence : IEnumerator, IEnumerable
+    {
+        Random rnd = new Random();
+        int length;
+        int maxValue;
+        int position = 0;
+
+        public RandomSequence(int length, int maxValue)
+        {
+            this.length = length;
+            this.maxValue = maxValue;
+        }
+        public object Current => rnd.Next(maxValue);
+
+        public bool MoveNext()
+        {
+            position++;
+            return position <= length;
+        }
+
+        public void Reset()
+        {
+            position = 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var weekday in new Week())
+            {
+                Console.WriteLine(weekday);
+            }
+
+            var randomSeq = new RandomSequence(20, 100);
+            foreach (var n in randomSeq) //
+            {
+                Console.Write(n + " ");
+            }
+
+            Console.WriteLine();
+
+            // foreach - синтаксический сахар для while
+            randomSeq.Reset();
+            while (randomSeq.MoveNext())
+            {
+                var n = randomSeq.Current;
+                Console.Write(n + " ");
+            }
+        }
+    }
+}
---
+++ GenericTree/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericTree
+{
+    class TreeNode<T>
+    {
+        public T Value { get; set; }
+        public List<TreeNode<T>> Children { get; }
+
+        public TreeNode(T value)
+        {
+            Value = value;
+            Children = new List<TreeNode<T>>();
+        }
+
+        public TreeNode(T value, List<TreeNode<T>> children)
+        {
+            Value = value;
+            Children = children;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(Value.ToString());
+            foreach (var child in Children)
+            {
+                foreach (string line in child.ToString().Split(Environment.NewLine))
+                {
+                    sb.Append(Environment.NewLine);
+                    sb.Append("  ");
+                    sb.Append(line);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+
+    class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+
+        public Tree(T rootValue)
+        {
+            Root = new TreeNode<T>(rootValue);
+        }
+
+        public override string ToString()
+        {
+            return Root.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<string> foodTree = new Tree<string>("Продукты");
+            foodTree.Root.Children.Add(
+                new TreeNode<string>("Хлеб")
+            );
+            foodTree.Root.Children.Add(
+                new TreeNode<string>("Овощи",
+                    new List<TreeNode<string>>()
+                    {
+                        new TreeNode<string>("Капуста"),
+                        new TreeNode<string>("Томаты",
+                            new List<TreeNode<string>>()
+                            {
+                                new TreeNode<string>("Черри"),
+                                new TreeNode<string>("Сливовидные")
+                            }
+                        ),
+                        new TreeNode<string>("Огурцы"),
+                    }
+                )
+            );
+            foodTree.Root.Children.Add(
+                new TreeNode<string>("Молочные продукты",
+                    new List<TreeNode<string>>()
+                    {
+                        new TreeNode<string>("Молоко"),
+                        new TreeNode<string>("Кефир"),
+                        new TreeNode<string>("Сметана"),
+                        new TreeNode<string>("Творог"),
+                        new TreeNode<string>("Йогурт"),
+                    }
+                )
+            );
+
+            Console.WriteLine(foodTree);
+        }
+    }
+}
